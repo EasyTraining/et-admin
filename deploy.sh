195cb6963acdb0529c0554b0959bbc84083647a4
rmlzy <rmlzy@outlook.com>
2020-12-14 21:40:48 +0800

    feat: 调整打包脚本

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 rm -rf dist & npm run build
 
 ## 删除旧代码
-ssh root@101.200.215.84 "rm -rf /home/et-admin/*; exit;"
+ssh root@101.200.215.84 "rm -rf /home/et-admin-dist/*; exit;"
 
 ### 上传文件
-scp -r ./dist/* root@101.200.215.84:/home/et-admin
+scp -r ./dist/* root@101.200.215.84:/home/et-admin-dist
 
 echo "✅发布成功"
 exit
